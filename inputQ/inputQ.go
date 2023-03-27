28319fa258354b6d66260bb847a116316ece8a7e
hkumar <hkumar@infinera.com>
2023-03-28 00:56:53 +0530

    Added inputQ design pattern

--- inputQ/inputQ.go
+++ inputQ/inputQ.go
@@ -1,20 +1,29 @@
 package inputq
 
 
-type counter struct {
-	inputQ chan interface{}
-	count int
+var counter = 0;
+var inputQ = make(chan interface{});
+
+type increment struct {
+	incrementBy int
+	responseChan chan int
 }
 
-func (c *counter) Initialize() {
-	c.inputQ = make(chan interface{}, 0)
-	c.count = 0
+func Increment(incrementBy int) int {
+	responseChan := make(chan int, 1);
+	inputQ <-increment{incrementBy: incrementBy, responseChan: responseChan}
+	return <-responseChan;
 }
 
-func (c *counter) Increment() {
-
+func ProcessInputQ() {
+	for {
+		item := <-inputQ;
+		v := item.(increment);
+		inc(v.incrementBy, v.responseChan)
+	}
 }
 
-func processInputQ() {
-
+func inc(incrementBy int, responseChan chan int){
+	counter += incrementBy;
+	responseChan <- counter;
 }
