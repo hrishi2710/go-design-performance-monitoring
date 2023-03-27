f945504a49712bcafc6058f2151e714b7c0e5490
hkumar <hkumar@infinera.com>
2023-03-28 01:28:07 +0530

    Added 2 modes to run the program in: mutex, inputQ

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"go-design-performance-monitoring/mutex"
 	pb "go-design-performance-monitoring/proto/counter"
 	"log"
 	"net"
 
-	mutex "go-design-performance-monitoring/mutex"
+	inputQ "go-design-performance-monitoring/inputQ"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -15,6 +17,7 @@ import (
 )
 
 const grpcServerPort = "50055"
+var program_mode string
 
 type CounterServer struct {
 	pb.UnimplementedIncrementCounterServer
@@ -25,13 +28,30 @@ func (s *CounterServer) Increment(ctx context.Context, req *pb.IncrementBy) (*pb
 	if req.Value < 0 {
 		return nil, status.Errorf(codes.InvalidArgument, "Value cannot be negative")
 	}
-	result := mutex.Increment(int(req.Value))
+	var result int;
+	if program_mode == "mutex" {
+		result = mutex.Increment(int(req.Value))
+	} else if program_mode == "inputQ" {
+		result = inputQ.Increment(int(req.Value))
+	}
 	msg := fmt.Sprintf("Incremented value is %d", result)
 	return &pb.Status{Message: msg}, nil
 }
 
 func main() {
 	fmt.Println("Inside main")
+	var mode *string = flag.String("mode", "", "Specify in which mode to run the server. Possible values are 'mutex' or 'inputQ'")
+	flag.Parse()
+	fmt.Println("mode: ", program_mode)
+	program_mode = *mode
+	fmt.Println(program_mode != "mutex")
+	if program_mode == "" || (program_mode != "inputQ" && program_mode != "mutex") {
+		log.Fatalf("Specify in which mode to run the server. Possible values are 'mutex' or 'inputQ'")
+	}
+	fmt.Println("Running the program in mode: ", program_mode)
+	if program_mode == "inputQ" {
+		go inputQ.ProcessInputQ()
+	}
 	lis, err := net.Listen("tcp", ":"+grpcServerPort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
